37e47ad6fb849aac14bdfabe56d2b1d360f8a46e
Marco Antônio <marcoantonio_fp@usp.br>
2021-01-21 17:16:01 -0300

    created project

---
+++ src/main/java/com/example/wsmongosb/WsmongosbApplication.java
@@ -0,0 +1,13 @@
+package com.example.wsmongosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsmongosbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsmongosbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wsmongosb/WsmongosbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wsmongosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsmongosbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
